d6445834216e7a157cc691019e48048802297e7b
diabolik99 <99539572+diabolik99@users.noreply.github.com>
2022-11-03 15:39:39 +0300

    project completed and explanation added

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()//soru getirme fonksiyonu
     }
+    //** Set title: başlığı ayarla
+    //** Current title: mevcut başlık
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)//nextStory fonksiyonundaki userChoice seçilen butona eşittir
+        updateUI()
+    }
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()//labela soruyu yazmak için fonksiyonu çağır
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)//fonksiyon çağır
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+//başlık seçenek 1,2 ve seçenek kaderi 1,2 diye sabitler oluşturduk.
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+}
